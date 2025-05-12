e60cc26f56c0ced092f8946b1c2bac95251295f6
Mogili Deepika <mogilideepika218@gmail.com>
2025-05-12 19:28:43 +0530

    modified scripts

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,19 +1,10 @@
-import { prisma } from "@repo/db/client";
-
 export default async function Home() {
-  const user = await prisma.user.findFirst();
 
-  if (!user) {
-    return <div>NO USER FOUND</div>;
-  }
   return (
     <div>
       <h1>Hello From Frontend...</h1>
       <h2>username:</h2>
-      <p>{user.username}</p>
-      <br />
-      <h2>password:</h2>
-      <p>{user.password}</p>
+      <h4>Hello GUys Its me</h4>
     </div>
   );
 }
